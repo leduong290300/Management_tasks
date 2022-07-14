d0a60965c9e76934049914b30859819f5865614f
leduong2903 <leduong290300@gmail.com>
2022-07-14 17:01:59 +0700

    Work dashboard

--- client/src/reducer/taskReducer.js
+++ client/src/reducer/taskReducer.js
@@ -15,6 +15,34 @@ export const taskReducer = (state, action) => {
         tasksLoading: false,
       };
 
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: [...state.tasks, payload],
+      };
+
+    case "DELETE_TASK":
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== payload),
+      };
+
+    case "FIND_TASK":
+      return {
+        ...state,
+        task: payload,
+      };
+
+    case "UPDATE_TASK":
+      const newTasks = state.tasks.map((task) =>
+        task.id === payload.id ? payload : task,
+      );
+
+      return {
+        ...state,
+        tasks: newTasks,
+      };
+
     default:
       return state;
   }
