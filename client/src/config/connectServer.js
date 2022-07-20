60f9d32ffc6e0b39055486d234eb1663ab069cc4
leduong2903 <leduong290300@gmail.com>
2022-07-20 10:31:22 +0700

    1

--- client/src/config/connectServer.js
+++ client/src/config/connectServer.js
@@ -1,2 +1,4 @@
 export const apiUrl =
-  process.env.NODE_ENV !== "production" ? "http://localhost:8000/api/v1" : "";
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:8000/api/v1"
+    : "https://management-tasks-app.herokuapp.com/api/v1";
